9c54ff3822df009c730595cacd6dbfc626bfba6e
Hamendo <rudranil@hamendotechnologies.com>
2025-08-14 00:32:57 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,24 +4,25 @@ import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 
 import AdminDashboard from './components/admin/AdminDashboard';
 import AdminLogin from './components/admin/AdminLogin';
+import GoogleFormPage from './components/admin/GoogleFormPage';
 import PerformanceView from './components/admin/PerformanceView';
 import DailyPerformanceForm from './components/employee/DailyPerformanceForm';
 import EmployeeDashboard from './components/employee/EmployeeDashboard';
 import EmployeeLogin from './components/employee/EmployeeLogin';
-import HomePage from './pages/HomePage';
 
 console.log(React);
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<EmployeeLogin />} />
 
         <Route path="/admin/login" element={<AdminLogin />} />
         <Route path="/admin/dashboard" element={<AdminDashboard />} />
         <Route path="/admin/performance" element={<PerformanceView />} />
+        <Route path="/admin/form-page" element={<GoogleFormPage />} />
 
-        <Route path="/employee/login" element={<EmployeeLogin />} />
+        {/* <Route path="/employee/login" element={<EmployeeLogin />} /> */}
         <Route path="/employee/dashboard" element={<EmployeeDashboard />} />
         <Route path="/employee/daily-performance" element={<DailyPerformanceForm />} />
       </Routes>
